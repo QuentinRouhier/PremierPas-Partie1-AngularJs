05c92bdcb935711fa561e25d1ea4a1d628e3a51f
QuentinR <quentin.rouhier@gmail.com>
2017-03-23 11:17:00 +0100

    ng-app='' pour les calcul

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name exercice1App
+ * @description
+ * # exercice1App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('exercice1App', []);
---
+++ newjavascript.js
@@ -0,0 +1,17 @@
+            var app = angular.module("app", []);
+
+            app.controller("headerCtrl", function ($scope) {
+                //...    
+            });
+
+            app.controller("footerCtrl", function ($scope) {
+                //...    
+            });
+
+            app.controller("menuCtrl", function ($scope) {
+                //...    
+            });
+
+            app.controller("contentCtrl", function ($scope) {
+                //...    
+            });
